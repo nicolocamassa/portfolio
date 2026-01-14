c8d0ac4c651b0518f77ddb2cbca428829de0d2c4
nicolocamassa <camassanicolo03@gmail.com>
2026-01-14 13:12:46 +0100

    chore: add tailwind-variants

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,7 +1,12 @@
+import { ButtonBackground } from "../ui/Buttons"
+
+ButtonBackground
+
 export default function Header(){
     return (
         <header className="w-full h-15 bg-amber-600 px-3 md:px-5">
             <div className="max-w-[1750px] h-full bg-amber-300 mx-auto">
+                <ButtonBackground></ButtonBackground>
             {/* * Content */}Header
             </div>
         </header>
