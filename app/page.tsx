5e681f7ec102609e6a99963b147e0bdc351ec325
nicolocamassa <camassanicolo03@gmail.com>
2026-01-14 12:15:26 +0100

    Fix 404 home route + font fallback warning

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "./components/HeroSection";
+import ProjectSection from "./components/ProjectSection";
+import AboutSection from "./components/AboutSection";
+
+export default function Page() {
+  return (
+    <main>
+      <HeroSection />
+      <ProjectSection />
+      <AboutSection />
+    </main>
+  );
+}
