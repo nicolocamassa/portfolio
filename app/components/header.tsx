c739d88da5a22fcd2228cefa1df3bb697a718e38
nicolocamassa <camassanicolo03@gmail.com>
2026-01-14 12:20:01 +0100

    feat: add CTA links

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,18 @@
+import { LogoText } from "./SVG/Logo"
+
 export default function Header(){
     return (
-        <header className="w-full h-15 bg-amber-600 px-3 md:px-5">
-            <div className="max-w-[1750px] h-full bg-amber-300 mx-auto">
-            {/* * Content */}Header
+        <header className="w-full h-15 bg-amber-600 px-3 md:px-5 sticky top-0 z-999">
+            <div className="max-w-[1750px] h-full bg-amber-300 mx-auto flex items-center justify-between">
+                <div className="logoContainer flex items-center gap-2">
+                    <LogoText></LogoText>
+                </div>
+
+                <nav className="text-sm flex items-center gap-2">
+                    <a href="#test">Contattami</a>
+                    <a href="#test" className="bg-black px-3 py-2 text-white rounded-lg">Scarica CV</a>
+                </nav>
+                
             </div>
         </header>
     )
