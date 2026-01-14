21618218450e8f3156f4566a1f21d13602d0afa6
nicolocamassa <camassanicolo03@gmail.com>
2026-01-14 12:31:23 +0100

    fix: header import path fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Parkinsans, Fustat } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./components/header";
 import Footer from "./components/Footer";
 
 const ParkinSans = Parkinsans({
