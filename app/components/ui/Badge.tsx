a460a267abab0ea8c6d37d22a34ef6dae2c9ea22
nicolocamassa <camassanicolo03@gmail.com>
2026-01-26 15:55:10 +0100

    feat: add base layout for Reading info banner component

--- app/components/ui/Badge.tsx
+++ app/components/ui/Badge.tsx
@@ -1,12 +1,13 @@
 import { tv, type VariantProps } from "tailwind-variants";
+import { CircleDashed, LoaderCircle, CircleCheck } from "lucide-react";
 
 const badge = tv({
-  base: "inline-flex flex-none items-center justify-center rounded-full text-wrap",
+  base: "inline-flex flex-none items-center gap-1 justify-center rounded-full text-wrap [&_svg]:hidden",
   variants: {
     type: {
       normal: "bg-[var(--text-highlight-15)] text-[var(--text-highlight)]",
       list: "rounded-lg border border-[var(--border-default)] font-semibold [&_svg]:w-5 [&_svg]:text-[var(--brand)] gap-2", /* TODO: Cambiare le dimensioni dei testi */
-      svg: "border border-[var(--border-default)] text-[var(--text-primary)]",
+      svg: "border border-[var(--border-default)] text-[var(--text-primary)] [&_svg]:block",
     },
     size: {
       xs: "text-[10px] px-4 py-1",
@@ -16,6 +17,11 @@ const badge = tv({
       xl: "text-sm px-6 py-3",
       listBase: "text-sm px-5 py-2"
     },
+    color: {
+      draft: "bg-gray-100 text-gray-700 font-bold [&_svg]:block leading-none py-2.5 px-2.5",
+      progress: "bg-yellow-100 text-yellow-700 font-bold [&_svg]:block leading-none py-2.5 px-2.5",
+      completed: "bg-green-100 text-green-700 font-bold [&_svg]:block leading-none py-2.5 px-2.5"
+    }
   },
   compoundVariants: [
     { type: "svg", class: "rounded-2xl" },
@@ -28,6 +34,20 @@ const badge = tv({
 
 export type BadgeProps = { children: React.ReactNode; className?: string } & VariantProps<typeof badge>;
 
-export default function Badge({ children, size, type, className }: BadgeProps) {
-  return <span className={badge({ size, type, className })}>{children}</span>;
+export default function Badge({ children, size, type, className, color }: BadgeProps) {
+  const renderIcon = () => {
+    if (color === "draft"){
+      return <CircleDashed className="self-center" size={17}/>
+    } 
+    else if (color === "progress"){
+      return <LoaderCircle className="self-center" size={17}/>
+    } 
+    else if (color === "completed"){
+      return <CircleCheck className="self-center" size={17}/>
+    }
+  };
+
+  return <span className={badge({ size, type, className, color })}>
+    {renderIcon()}{children}
+  </span>;
 }
--- app/components/ui/ReadingInfoBanner.tsx
+++ app/components/ui/ReadingInfoBanner.tsx
@@ -1,6 +1,38 @@
+import StackIcon from "tech-stack-icons"
+import Badge from "./Badge"
+
 export default function ReadingInfoBanner(){
     return (
-        /*  */
-        <div className="p-5 border border-(--border-default) rounded rounded-lg">ciao</div>
+        /* TODO: Il banner deve essere un componente a parte con padding e margin di default */
+        <div className="flex items-center justify-between pb-20">
+            <div className="text-center flex flex-col gap-1">
+                <h5 className="text-md text-(--text-secondary)">Tempo di lettura</h5>
+                <span className="font-sans font-bold text-2xl">5'</span>
+            </div>
+
+            <div className="w-px h-12 bg-gray-200"></div> 
+
+            <div className="text-center flex flex-col gap-1">
+                <h5 className="text-md text-(--text-secondary)">Tech Stack</h5>
+                <div className="flex justify-center gap-2 mt-1">
+                    <StackIcon name="react" className="h-6 w-6 overflow-visible" />
+                    <StackIcon name="nextjs2" className="h-6 w-6 overflow-visible" />
+                </div>
+            </div>
+
+            <div className="w-px h-12 bg-gray-200"></div> 
+
+            <div className="text-center flex flex-col gap-1">
+                <h5 className="text-md text-(--text-secondary)">Data di inizio</h5>
+                <Badge className="font-semibold">26.01.2026</Badge>
+            </div>
+
+            <div className="w-px h-12 bg-gray-200"></div> 
+
+            <div className="text-center flex flex-col gap-1">
+                <h5 className="text-md text-(--text-secondary)">Stato</h5>
+                <span className="text-2xl"><Badge color="completed">Completato</Badge></span>
+            </div>
+        </div>
     )
 }
